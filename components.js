f99077f8f3a1c1d3709e69732ce5f084685e00d8
Jisan Jung <jjjung13@gmail.com>
2020-06-24 23:56:22 -0400

    notes on components

--- components.js
+++ components.js
@@ -3,6 +3,7 @@
 
 // component structure
 import React, { Component } from "react";
+import { func } from "prop-types";
 // you can import css, images, other components, whatever
 
 // class name should be file name
@@ -21,3 +22,43 @@ class App extends Component {
 
 // this allowsthe component "App" to be imported into index.js
 export default App;
+
+
+// good conventions
+// it's always good to write methods in the class (above the render method)
+// then, use the method by doing this.method() in curly braces in jsx
+
+// constructors
+constructor() {
+    // you MUST call super method
+    super();
+    this.name = "jason";
+    // this refers to your component
+}
+
+
+// 2 types of components, functional and class
+// functional
+import React from "react";
+
+// bsically just a function that returns jsx
+const Hello = () => <h1>Hello World</h1>
+
+export default Hello
+
+// class components
+import React, { Component } from "react";
+// you must include the Component when using class components
+
+// bsically js class that returns jsx
+class Hello extends Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default Hello
+// use functional components as much as possible
+// only use class components when dealing with state
