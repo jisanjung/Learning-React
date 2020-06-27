b3efe2d57b1429f0dcfcbab0266b8e891897b767
Jisan Jung <jjjung13@gmail.com>
2020-06-27 12:40:46 -0400

    styling notes

---
+++ styling.js
@@ -0,0 +1,5 @@
+// inline styling 
+// similar to styling dom elements in js
+<div style={{ backgrouncColor: "#e3e3e3" }}></div>
+
+// it's good convention to assign the styling object to a variable
