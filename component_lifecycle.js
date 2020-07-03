40433444e623ebba7623facfaef4bfa7aca9b478
Jisan Jung <jjjung13@gmail.com>
2020-07-03 13:20:49 -0400

    notes of component lifecycle

---
+++ component_lifecycle.js
@@ -0,0 +1,10 @@
+// most commonly used lifecycle methods
+
+componentDidMount
+// executed immediately after rendering
+
+componentDidUpdate
+// After component's updates are flushed to DOM
+
+componentWillUnmount
+// Immediately before removing component from DOM
