f71d96e714bd1b8a12ec8918199cbfb25aee5c3a
Jisan Jung <jjjung13@gmail.com>
2020-07-03 13:27:54 -0400

    additional lifecycle notes to ellaborate

--- component_lifecycle.js
+++ component_lifecycle.js
@@ -1,10 +1,12 @@
 // most commonly used lifecycle methods
 
-componentDidMount
+componentDidMount()
 // executed immediately after rendering
+// constructor fires first, then render method and this method is third
 
-componentDidUpdate
+componentDidUpdate(prevProps, prevState)
 // After component's updates are flushed to DOM
+// gives back information about the previous props or previous state when this function is executed
 
-componentWillUnmount
+componentWillUnmount()
 // Immediately before removing component from DOM
